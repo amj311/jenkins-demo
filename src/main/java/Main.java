c6b6dcb48546bd6f41496a61f0a4aaf8f7e29e76
Arthur Judd <amjudd315@gmail.com>
2021-10-22 23:42:29 -0600

    supports looping input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,63 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        String command = args[0];
-
+    public static void main(String[] arg) {
+        Scanner userIn = new Scanner(System.in);
+        String command;
         Calculator calc = new Calculator();
+        boolean doLoop = true;
 
-        switch (command) {
-            case "add":
-                int add1 = Integer.parseInt(args[1]);
-                int add2 = Integer.parseInt(args[2]);
-                System.out.println(calc.add(add1, add2));
-                break;
+        System.out.println("Enter a command!");
 
-            case "subtract":
-                int sub1 = Integer.parseInt(args[1]);
-                int sub2 = Integer.parseInt(args[2]);
-                System.out.println(calc.subtract(sub1, sub2));
-                break;
+        while (doLoop && (command = userIn.nextLine().toLowerCase()) != null) {
+            String[] args = command.split(" ");
+            switch (args[0]) {
+                case "add":
+                    int add1 = Integer.parseInt(args[1]);
+                    int add2 = Integer.parseInt(args[2]);
+                    System.out.println(calc.add(add1, add2));
+                    break;
 
-
-            case "multiply":
-                int mult1 = Integer.parseInt(args[1]);
-                int mult2 = Integer.parseInt(args[2]);
-                System.out.println(calc.multiply(mult1, mult2));
-                break;
+                case "subtract":
+                    int sub1 = Integer.parseInt(args[1]);
+                    int sub2 = Integer.parseInt(args[2]);
+                    System.out.println(calc.subtract(sub1, sub2));
+                    break;
 
 
+                case "multiply":
+                    int mult1 = Integer.parseInt(args[1]);
+                    int mult2 = Integer.parseInt(args[2]);
+                    System.out.println(calc.multiply(mult1, mult2));
+                    break;
 
-            case "divide":
-                int div1 = Integer.parseInt(args[1]);
-                int div2 = Integer.parseInt(args[2]);
-                System.out.println(calc.divide(div1, div2));
-                break;
 
-            case "binary":
-                int bin = Integer.parseInt(args[1]);
-                System.out.println(calc.intToBinaryNumber(bin));
-                break;
 
-            case "fib":
-                int fib = Integer.parseInt(args[1]);
-                System.out.println(calc.fibonacciNumberFinder(fib));
-                break;
+                case "divide":
+                    int div1 = Integer.parseInt(args[1]);
+                    int div2 = Integer.parseInt(args[2]);
+                    System.out.println(calc.divide(div1, div2));
+                    break;
 
-            default:
-                System.out.println("Invalid command.");
+                case "binary":
+                    int bin = Integer.parseInt(args[1]);
+                    System.out.println(calc.intToBinaryNumber(bin));
+                    break;
+
+                case "fib":
+                    int fib = Integer.parseInt(args[1]);
+                    System.out.println(calc.fibonacciNumberFinder(fib));
+                    break;
+
+                case "exit":
+                    doLoop = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid command.");
+
+        };
+
 
         }
     }
